798a45f891aa2003721cf9ab48eed9e6751b7254
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2021-08-01 06:25:56 -0300

    grid view com unidades

--- lib/screens/unit.dart
+++ lib/screens/unit.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Unit extends StatefulWidget {
@@ -10,8 +11,45 @@ class Unit extends StatefulWidget {
 class _UnitState extends State<Unit> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Em construção'),
+    return FutureBuilder<QuerySnapshot>(
+      future:
+          Firestore.instance.collection('cover').orderBy('pos').getDocuments(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData)
+          return SliverToBoxAdapter(
+            child: Container(
+              height: 200,
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ),
+          );
+        else
+          return Padding(
+              padding: EdgeInsets.symmetric(vertical: 30),
+              child: GridView.count(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 30,
+                children: snapshot.data!.documents.map((doc) {
+                  return GestureDetector(
+                    child: GridTile(
+                      child: Image.network(
+                          'https://drive.google.com/uc?export=view&id=' +
+                              doc.data!['img']),
+                    ),
+                    onTap: () {
+                      print('alo');
+                    },
+                  );
+                }).toList(),
+              ));
+      },
     );
   }
 }
+
+/*
+
+ */
